a6500aa6936b39bf553cba890300443fb58f9c91
Facundo Serna <facuserna93@gmail.com>
2021-08-22 20:04:04 -0300

    Permito distintas proporciones y tamaño de pantalla

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -9,9 +9,9 @@ player = MontecarloPlayer(tic_tac_toe, selection_function, expansion_function, r
 
 class Tile(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, surface_width, surface_height):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([150, 150])
+        self.image = pygame.Surface([surface_width, surface_height])
         self.image.fill(white)
         self.rect = self.image.get_rect()
 
@@ -24,7 +24,9 @@ class GameRenderManager():
         self.window_height = window_height
         self.window = pygame.display.set_mode((window_width, window_height))
         self.ia_thinking = False
+        self.window_relations = self.calculate_window_relations()
         self.fill_tile_list()
+
 
     def get_tile_to_draw(self, grid_cordinates):
         tile_index = 3 * grid_cordinates[1] + grid_cordinates[0]
@@ -34,31 +36,59 @@ class GameRenderManager():
         return tile
 
     def draw_figure(self, figure, grid_cordinates):
-
         tile = self.get_tile_to_draw(grid_cordinates)
 
         if figure == 'circle':
-            self.draw_circle(tile)
+            self.draw_ellipse(tile)
         elif figure == 'rect':
             self.draw_square(tile)
         elif figure == 'cross':
             self.draw_cross(tile)
 
     def draw_circle(self, tile):
-        pygame.draw.circle(self.window, green, (tile.rect.x + 75, tile.rect.y + 75), 50)
+        margin_x = self.window_relations.get('circle_margin_x')
+        margin_y = self.window_relations.get('circle_margin_y')
+        line_width = self.window_relations.get('circle_line_width')
+        radius = self.window_relations.get('circle_radius')
+        pygame.draw.circle(self.window, green, (tile.rect.x + margin_x, tile.rect.y + margin_y), radius, line_width)
+
+    def draw_ellipse(self, tile):
+        margin_x = self.window_relations.get('cross_margin_x')
+        margin_y = self.window_relations.get('cross_margin_y')
+        width = self.window_relations.get('cross_width')
+        height = self.window_relations.get('cross_height')
+        width_line = self.window_relations.get('cross_width_line')
+
+        pygame.draw.ellipse(self.window, green, (tile.rect.x + margin_x, tile.rect.y + margin_y, width, height), width_line)
+
 
     def draw_square(self, tile):
-        pygame.draw.rect(self.window, red, (tile.rect.x + 25, tile.rect.y + 25, 100, 100))
+        margin_x = self.window_width / 22
+        margin_y = self.window_height / 22
+        width = self.window_width / 5.5
+        height = self.window_height / 5.5
+        pygame.draw.rect(self.window, red, (tile.rect.x + margin_x, tile.rect.y + margin_y, width, height))
 
     def draw_cross(self, tile):
-        pass
+        margin_x = self.window_relations.get('cross_margin_x')
+        margin_y = self.window_relations.get('cross_margin_y')
+        width = self.window_relations.get('cross_width')
+        height = self.window_relations.get('cross_height')
+        width_line = self.window_relations.get('cross_width_line')
+
+        first_line_start = (tile.rect.x + margin_x, tile.rect.y + margin_y)
+        first_line_end = (first_line_start[0] + width, first_line_start[1] + height)
+        second_line_start = (tile.rect.x + margin_x, tile.rect.y + margin_y + height)
+        second_line_end = (first_line_start[0] + width, first_line_start[1])
+        pygame.draw.line(self.window, red, first_line_start, first_line_end, width_line)
+        pygame.draw.line(self.window, red, second_line_start, second_line_end, width_line)
 
     def fill_tile_list(self):
         for row in range(3):
             for column in range(3):
-                tile = Tile()
-                tile.rect.x = 25 + 175 * row
-                tile.rect.y = 25 + 175 * column
+                tile = Tile(self.window_relations.get('tile_width'), self.window_relations.get('tile_height'))
+                tile.rect.x = self.window_relations.get('tile_margin_x') + self.window_relations.get('tile_rect_x_multiplier') * row
+                tile.rect.y = self.window_relations.get('tile_margin_y') + self.window_relations.get('tile_rect_y_multiplier') * column
                 self.tile_list.add(tile)
 
     def draw_tiles(self):
@@ -69,11 +99,28 @@ class GameRenderManager():
 
     def get_player_figure(self, is_player_one):
         if is_player_one:
-            return 'rect'
+            return 'cross'
         else:
             return 'circle'
 
-
+    def calculate_window_relations(self):
+        window_relations = {}
+        window_relations['circle_margin_x'] = self.window_width * 3 / 22
+        window_relations['circle_margin_y'] = self.window_height * 3 / 22
+        window_relations['circle_radius'] = int((self.window_width + self.window_width) * 6 / 110)
+        window_relations['circle_line_width'] = int((self.window_width + self.window_width) / 137.5)
+        window_relations['cross_margin_x'] = self.window_width / 22
+        window_relations['cross_margin_y'] = self.window_height / 22
+        window_relations['cross_width'] = self.window_width / 5.5
+        window_relations['cross_height'] = self.window_height / 5.5
+        window_relations['cross_width_line'] = (self.window_width + self.window_height) // 110
+        window_relations['tile_width'] = self.window_width * 3 // 11
+        window_relations['tile_height'] = self.window_height * 3 // 11
+        window_relations['tile_margin_x'] = self.window_width / 22
+        window_relations['tile_margin_y'] = self.window_height / 22
+        window_relations['tile_rect_x_multiplier'] = self.window_width * 7 / 22
+        window_relations['tile_rect_y_multiplier'] = self.window_height * 7 / 22
+        return window_relations
 
 
 def execute_ia_move(ia_player):
@@ -96,8 +143,8 @@ def find_ia_action_node(ia_player, row_action, column_action):
             action_node = child
     return action_node
 
-window_width = 550
-window_height = 550
+window_width = 300
+window_height = 500
 white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
@@ -105,7 +152,7 @@ green = (0, 255, 0)
 pygame.init()
 pygame.display.set_caption('Tic Tac Toe')
 
-game_manager_render = GameRenderManager(550, 550)
+game_manager_render = GameRenderManager(window_width, window_height)
 
 game_manager_render.draw_tiles()
 
